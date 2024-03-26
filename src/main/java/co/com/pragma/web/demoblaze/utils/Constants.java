19107cfb606d8ddb4bd1b4a80b4b042d69002605
Daniel Henao <dhenao693@gmail.com>
2024-03-26 15:55:07 -0500

    Creación primeras clases

---
+++ src/main/java/co/com/pragma/web/demoblaze/utils/Constants.java
@@ -0,0 +1,11 @@
+package co.com.pragma.web.demoblaze.utils;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class Constants {
+    public static final String  DEFAULT_USER = "DEFAULT";
+    public static final String  USER_IN_MEMORY = "USER";
+    public static final String  CARD_IN_MEMORY = "CARD";
+    public static final String URL_HOME = "https://www.demoblaze.com/";
+}
